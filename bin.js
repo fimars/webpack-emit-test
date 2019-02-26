466fc4c9bc8c90f0e15d40baf558e82b873bd5f5
fimars <foru.fy@gmail.com>
2019-02-26 18:17:13 +0800

    add case

--- bin.js
+++ bin.js
@@ -3,19 +3,27 @@ const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config');
 const fs = require('fs');
 
-fs.writeFileSync('module-c.js', 'export const c = "asasasasasasasasasasasasasasas";');
+fs.writeFileSync('module-c.js', 'export const c = "we";');
 
-const devServerOptions = Object.assign({}, webpackConfig.devServer, {
-  stats: {
-    colors: true,
-  },
-});
+function holdTheDevServer () {
+  const devServerOptions = Object.assign({}, webpackConfig.devServer, {
+    stats: {
+      colors: true,
+    },
+  });
 
-WebpackDevServer.addDevServerEntrypoints(webpackConfig, devServerOptions);
+  WebpackDevServer.addDevServerEntrypoints(webpackConfig, devServerOptions);
 
-const compiler = Webpack(webpackConfig);
-const server = new WebpackDevServer(compiler, devServerOptions);
+  const compiler = Webpack(webpackConfig);
+  const server = new WebpackDevServer(compiler, devServerOptions);
 
-server.listen(8080, '127.0.0.1', () => {
-  console.log('Starting server on http://localhost:8080');
-});
+  server.listen(8080, '127.0.0.1', () => {
+    console.log('Starting server on http://localhost:8080');
+  });
+}
+
+// # Log twice, the module-c update late then first log.
+// holdTheDevServer();
+
+// # work well
+setTimeout(() => holdTheDevServer(), 1000);
